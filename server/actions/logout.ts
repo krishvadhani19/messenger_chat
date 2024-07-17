87c2dbe5c6efaca5e88cd151178fbdac1f4b81c6
krishvadhani19 <krishvadhani7@gmail.com>
2024-07-17 14:20:05 +0530

    Handling url change on logout

--- server/actions/logout.ts
+++ server/actions/logout.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { signOut } from "@/auth";
+import { LOGIN_PAGE } from "@/routes";
 
 export const logout = async () => {
-  await signOut();
+  await signOut({ redirectTo: LOGIN_PAGE });
 };
