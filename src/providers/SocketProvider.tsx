35bf32cfee1312b3bdb845b534be7b22e731af8c
krishvadhani19 <krishvadhani7@gmail.com>
2024-09-10 14:11:48 -0400

    Socket setup done, working on chat functionality

--- src/providers/SocketProvider.tsx
+++ src/providers/SocketProvider.tsx
@@ -1,61 +1,34 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { io, Socket } from "socket.io-client";
+import { io } from "socket.io-client";
 import { SocketContext } from "@/contexts/SocketContext";
 
-interface SocketContextType {
-  socket: Socket | null;
-  isConnected: boolean;
-}
+const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL!;
+const socketInstance = io(NEXT_PUBLIC_SITE_URL);
 
 export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [socket, setSocket] = useState<Socket | null>(null);
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
   useEffect(() => {
-    const NEXT_PUBLIC_SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
-
-    if (!NEXT_PUBLIC_SITE_URL) {
-      console.error("NEXT_PUBLIC_SITE_URL is not defined");
-      return;
-    }
-
-    const socketInstance = io(NEXT_PUBLIC_SITE_URL!, {
-      path: "/api/sockets/io",
-      transports: ["websocket", "polling"],
-      addTrailingSlash: false,
-    });
-
-    socketInstance.on("connect", () => {
+    if (socketInstance) {
       setIsConnected(true);
-    });
-
-    socketInstance.on("disconnect", () => {
-      setIsConnected(false);
-    });
-
-    socketInstance.on("connect_error", (err: Error) => {
-      console.error("Socket connect error: ", err);
-      setIsConnected(false);
-    });
-
-    setSocket(socketInstance);
-
-    return () => {
-      socketInstance.disconnect();
-    };
+      socketInstance.on("message2", (data) => {
+        console.log("Received from SERVER ::", data);
+        // Execute any command
+      });
+    }
   }, []);
 
-  const contextValue: SocketContextType = {
-    socket,
-    isConnected,
-  };
-
   return (
-    <SocketContext.Provider value={contextValue}>
+    <SocketContext.Provider
+      value={{
+        socket: socketInstance,
+        isConnected,
+      }}
+    >
       {children}
     </SocketContext.Provider>
   );
