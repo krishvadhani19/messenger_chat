0098d5260a8404f0f723f7619e1b0cb9fe79713d
krishvadhani19 <krishvadhani7@gmail.com>
2024-09-11 13:30:22 -0400

    Next config changes not needed

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         source: "/api/sockets/io",
-        destination: "http://localhost:3000/api/sockets/io",
+        destination: "http://localhost:3000/api/socket",
       },
     ];
   },
